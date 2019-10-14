031088f09795f8f03ee8e64ea85eef9e5f441d8e
Eric Wustrow <ewust@colorado.edu>
2019-10-14 15:30:42 -0600

    add factorial exercise

---
+++ exercises/factorial.py
@@ -0,0 +1,83 @@
+
+from exercises import *
+
+############
+# Fib
+def check_factorial(asm):
+    new_start = '''.text
+    TEST_N: .word 3
+    _start:
+        movia   sp, 0x04000000
+        subi    sp, sp, 4
+
+        # polute callee's
+        movui    r16, 0xaaa2
+        movui    r17, 0xbbb4
+        movui    r18, 0xccc1
+        movui    r19, 0xddd0
+        movui    r20, 0xeee2
+        movui    r21, 0x131f
+        movui    r22, 0x831e
+        movui    r23, 0x918c
+
+        movia   r4, TEST_N
+        ldw     r4, 0(r4)
+        call    factorial
+        break
+    '''
+    hp = new_start + asm    # asm.replace('roll:', '_roll:') hmm..
+    nobj = nios2_as(hp.encode('utf-8'))
+    cpu = Nios2(obj=nobj)
+
+    tests = [(3, 6), (5, 120), (10, 3628800), (12, 479001600)]
+    feedback = ''
+    extra_info = ''
+    cur_test = 1
+    clobbered = set()
+    for n,ans in tests:
+        cpu.reset()
+        cpu.write_symbol_word('TEST_N', n)
+
+        instrs = cpu.run_until_halted(100000000)
+
+
+        # Check for clobbered registers first
+        # in case this is why they failed
+        clobs = cpu.get_clobbered()
+        if len(clobs) > 0:
+            for pc,rid in clobs:
+                if (pc,rid) not in clobbered:
+                    extra_info += 'Warning: Function @0x%08x clobbered r%d\n<br/>' % (pc, rid)
+                    clobbered.add((pc, rid))
+
+        # Check answer
+        their_ans = cpu.get_reg(2)
+        if their_ans != ans:
+            feedback += 'Failed test case %d: ' % cur_test
+            feedback += 'factorial(%d) returned %d, should have returned %d' %\
+                    (n, their_ans, ans)
+            feedback += get_debug(cpu, show_stack=True)
+            del cpu
+            return (False, feedback, extra_info)
+
+        feedback += 'Passed test case %d<br/>\n' % cur_test
+        cur_test += 1
+
+    del cpu
+    return (True, feedback, extra_info)
+
+Exercises.addExercise('factorial',
+    {
+        'public': True,
+        'title': 'Factorial',
+        'diff':  'medium',
+        'desc':  '''Factorial can be defined recursively as <code>f(n) = n*f(n-1)</code>, with a base case value of <code>f(0) = 1</code>.<br/><br/>
+        Your task is to write a function <code>factorial</code> which takes a single number <code>n</code> and returns <code>f(n)</code>.''',
+        'code':'''.text
+factorial:
+    # Write your code here
+
+    ret
+''',
+        'checker': check_factorial
+    })
---
+++ util.py
@@ -0,0 +1,113 @@
+
+import tempfile
+import subprocess
+import json
+from collections import defaultdict
+import struct
+
+def nios2_as(asm):
+    asm_f = tempfile.NamedTemporaryFile()
+    asm_f.write(asm)
+    asm_f.flush()
+
+    obj_f = tempfile.NamedTemporaryFile()
+
+    ########## Assemble
+    p = subprocess.Popen(['bin/nios2-elf-as', \
+                          asm_f.name, \
+                          '-o', obj_f.name],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if p.wait() != 0:
+        ret = 'Assembler error: %s' % p.stderr.read()
+        obj_f.close()
+        asm_f.close()
+        p.stdout.close()
+        p.stderr.close()
+        return ret
+
+    asm_f.close()
+    p.stdout.close()
+    p.stderr.close()
+
+
+    ######### Link
+    exe_f = tempfile.NamedTemporaryFile()
+    p = subprocess.Popen(['bin/nios2-elf-ld', \
+                          '-T', 'de10.ld', \
+                          obj_f.name, '-o', exe_f.name],
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
+    if p.wait() != 0:
+        ret = 'Linker error: %s' % p.stderr.read()
+        p.stderr.close()
+        p.stdout.close()
+        obj_f.close()
+        return ret
+
+    obj_f.close()
+    p.stdout.close()
+    p.stderr.close()
+
+    ######## objdump
+    p = subprocess.Popen(['./gethex.sh', exe_f.name], \
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if p.wait() != 0:
+        ret = 'Objdump error: %s' % p.stderr.read()
+        p.stderr.close()
+        p.stdout.close()
+        exe_f.close()
+        return ret
+
+    obj = json.loads(p.stdout.read().decode('ascii'))
+    p.stdout.close()
+    p.stderr.close()
+    exe_f.close()
+
+    return obj
+
+def get_debug(cpu, mem_len=0x100, show_stack=False):
+    out = '<br/>\n'
+    out += cpu.get_error()
+    out += '<br/>Memory:<br/><pre>'
+    out += cpu.dump_mem(0, mem_len)
+    out += '\nSymbols:\n' + cpu.dump_symbols()
+    out += '</pre>'
+    if show_stack:
+        sp = cpu.get_reg(27)
+        fp = cpu.get_reg(28)
+        out += '<br/>Stack:<br/><pre>'
+        out += 'sp = 0x%08x\nfp = 0x%08x\n\n' % (sp, fp)
+        diff = 0x04000000 - (sp-0x80)
+        out += cpu.dump_mem(sp-0x80, min(0x100, diff))
+        out += '\n</pre>'
+    return out
+
+def hotpatch(obj, new_start_asm):
+    hp = '.text\n'
+    # fill symbols
+    rev_map = defaultdict(list) # addr => [list_of_symbols]
+    # TODO: this will only work for word-aligned labels...
+    for s,addr in obj['symbols'].items():
+        #hp += '.equ %s, 0x%08x\n' % (s, addr)
+        if s != '_start':
+            rev_map[addr].append(s)
+
+    # fill bytes
+    p = bytes.fromhex(obj['prog'])
+    for i in range(len(p)>>2):
+        addr = 4*i
+        word, = struct.unpack('>I', p[4*i:4*i+4])
+        for sym in rev_map[addr]:
+            hp += '%s:\n' % sym
+        hp += ' .word 0x%08x\n' % (word)
+    hp += new_start_asm
+    return nios2_as(hp.encode('utf-8'))
+
+def require_symbols(obj, symbols):
+    if not(isinstance(obj, dict)):
+        return str(obj)
+    #if '_start' not in obj['symbols']:
+    for s in symbols:
+        if s not in obj['symbols']:
+            return '%s not found in memory (did you enter any instructions?)' % (s)
+    return None
+
