1ccb7e90da33c1bf692c183a586cbd3e5f398fbb
Elena Murray <elmu6301@colorado.edu>
2020-09-18 17:06:35 -0600

    Sum 1-N Autograder complete:

--- exercises/sum-1-N.py
+++ exercises/sum-1-N.py
@@ -15,71 +15,38 @@ def sum_1_to_N(N):
 # Set the LEDs to all on
 def check_sum_1_to_N(asm):
     obj = nios2_as(asm.encode('utf-8'))
+    r = require_symbols(obj, ['N', '_start'])
+    if r is not None:
+        return (False, r)
     cpu = Nios2(obj=obj)
-    
+    # print(sum_1_to_N(20))
     #variables 
     cur_test = 0
     feedback = ''
 
-    # Test case 0 (Test their value of N): 
-    # cpu.reset()
-    instrs = cpu.run_until_halted(10000)
-   
-    #verify that r4 is valid and is storing N
-    N = cpu.get_reg(4)
-    print(N)
-    if N <= 1:
-        feedback += 'Failed test case %d: ' % (cur_test)
-        feedback += 'N was either not found in <code>r4</code> or not an integer greater than 1.'
-        feedback += '<br/><br/>Memory:<br/><pre>'
-        feedback += cpu.dump_mem(0, 0x100)
-        feedback += '\nSymbols:\n' + cpu.dump_symbols()
-        feedback += '</pre>'
-        return (False, feedback)
-    
-    #check their answer
-    ans = sum_1_to_N(N)
-    their_ans = cpu.get_reg(2)
-    if ans != their_ans:
-        feedback += 'Failed test case %d: ' % (cur_test)
-        feedback += '<code>r2</code> should be %d for <b>N</b> = %d .' % (ans,N)
-        feedback += 'Your code produced <code>r2</code> = %d' % (their_ans)
-        feedback += '<br/><br/>Memory:<br/><pre>'
-        feedback += cpu.dump_mem(0, 0x100)
-        feedback += '\nSymbols:\n' + cpu.dump_symbols()
-        feedback += '</pre>'
-        return (False, feedback)
-    feedback += 'Passed test case %d<br/>\n' % (cur_test)
-    cur_test += 1
-    
-    # Test cases 
-    # test_cases = [(9,45)]
+    test_cases = [(10,55), (9, 45), (20,210)]
+    for N, ans in test_cases:
+        ## Reset and initialize
+        cpu.reset()
+        cpu.write_symbol_word('N', N)
 
-    # for N, ans in test_cases:
-    #     # Reset and Initialize
-    #     cpu.reset()
 
-    #     #Set r4 to n
-    #     cpu.set_reg(4,N)
+        # Run
+        instrs = cpu.run_until_halted(10000)
 
-    #     # Run
-    #     instrs = cpu.run_until_halted(10000)
-
-    #     # Check answer
-    #     their_ans = cpu.get_reg(2)
-    #     if their_ans != ans:
-    #         feedback += 'Failed test case %d: ' % (cur_test)
-    #         feedback += '<code>r2</code> should be %d for <b>N</b> = %d .' % (ans,N)
-    #         feedback += 'Your code produced <code>r2</code> = %d' % (their_ans)
-    #         feedback += '<br/><br/>Memory:<br/><pre>'
-    #         feedback += cpu.dump_mem(0, 0x100)
-    #         feedback += '\nSymbols:\n' + cpu.dump_symbols()
-    #         feedback += '</pre>'
-
-    #         return (False, feedback)
-    #     feedback += 'Passed test case %d<br/>\n' % (cur_test)
-    #     cur_test += 1
-  
+        # Check Answer
+        their_ans = cpu.get_reg(2)
+        if their_ans != ans:
+            feedback += 'Failed test case %d: ' % (cur_test)
+            feedback += '<code>r2</code> should be %d for <b>N</b> = %d .' % (ans,N)
+            feedback += 'Your code produced <code>r2</code> = %d' % (their_ans)
+            # feedback += '<br/><br/>Memory:<br/><pre>'
+            # feedback += cpu.dump_mem(0, 0x100)
+            feedback += '\nSymbols:\n' + cpu.dump_symbols()
+            feedback += '</pre>'
+            return (False, feedback)
+        feedback += 'Passed test case %d<br/>\n' % (cur_test)
+        cur_test += 1  
     return (True, feedback)
 
 
@@ -88,13 +55,23 @@ Exercises.addExercise('sum-1-N',
         'public': True,
         'title': 'Sum integers from 1-N',
         'diff':  'easy',
-        'desc': '''Sum the integers starting from 1 and ending at N, where N is an integer of your choice.<br/><br/>
-                   <b>N</b> is stored in <code>r4</code> and the <b>sum</b> is stored in <code>r2</code>''',
+        'desc': '''Sum the integers starting from 1 and ending at N.<br/><br/>
+                   Use <code>r4</code> to store <b>N</b> and <code>r2</code> to store the sum.
+                   The instructions to load <b>N</b> has been provided.
+                   \n<b>Note: Do not edit the provided code.</b>''',
         'code':'''.text
 _start:
+movia r4, N
+ldw r4, 0(r4)
+movi r2, 0
+
+# Your code here
 
 
+break
 
+.data
+N: .word 9 
 ''',
         'checker': check_sum_1_to_N
     })
