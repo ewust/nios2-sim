dd78f9214bfeec0a2e10a7f7450c77c22d4ae170
Eric Wustrow <ewust@colorado.edu>
2020-11-12 14:51:55 -0700

    migration

--- init_leader.py
+++ init_leader.py
@@ -2,5 +2,5 @@ import sqlite3
 db = sqlite3.connect('leaderboard.db')
 
 
-db.execute("CREATE TABLE leaders (id INTEGER PRIMARY KEY AUTOINCREMENT, user char(10) NOT NULL, ip TEXT, timestamp INTEGER, code BLOB, instructions INTEGER, size INTEGER, public bool NOT NULL)")
+db.execute("CREATE TABLE leaders (id INTEGER PRIMARY KEY AUTOINCREMENT, user char(10) NOT NULL, ip TEXT, timestamp INTEGER, code BLOB, instructions INTEGER, size INTEGER, time_us INTEGER, public bool NOT NULL)")
 db.commit()
