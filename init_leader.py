2b001585cbb92b91ff346e34befe3238df5e9d77
Eric Wustrow <ewust@colorado.edu>
2026-01-08 11:42:32 -0700

    init leaders adds users now

--- init_leader.py
+++ init_leader.py
@@ -1,6 +1,20 @@
 import sqlite3
+import sqlite3, csv
+
 db = sqlite3.connect('leaderboard.db')
+cur = db.cursor()
+
+# Make users table
+cur.execute("CREATE TABLE IF NOT EXISTS users (user char(10))")
+
+# Populate it
+with open("users.csv", newline="") as f:
+    for row in csv.reader(f):
+        cur.execute("INSERT INTO users (user) VALUES (?)", (row[0],))
 
 
-db.execute("CREATE TABLE leaders (id INTEGER PRIMARY KEY AUTOINCREMENT, user char(10) NOT NULL, ip TEXT, timestamp INTEGER, code BLOB, instructions INTEGER, size INTEGER, time_us INTEGER, public bool NOT NULL)")
+cur.execute("CREATE TABLE IF NOT EXISTS leaders (id INTEGER PRIMARY KEY AUTOINCREMENT, user char(10) NOT NULL, ip TEXT, timestamp INTEGER, code BLOB, instructions INTEGER, size INTEGER, time_us INTEGER, public bool NOT NULL)")
+
+
 db.commit()
+db.close()
